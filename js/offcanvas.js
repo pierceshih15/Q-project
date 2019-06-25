569955748daa1a0beb557f6e0227a5924d0ce7d8
pierce <pierceshih@gmail.com>
2019-06-26 02:08:01 +0800

    Update for landing page of Mobile and Tablet.

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,13 +1,25 @@
 $(function () {
   'use strict'
 
-  // 開啟 Header
+  // 開啟 Mobile-Header
   $('[data-toggle="offcanvas"]').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open');
+    $('#offcanvas').toggleClass('open');
   })
 
-  // 關閉 Header
-  $('.icon-close').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open');
+  // 關閉 Mobile-Header
+  $('.icon-close-header').on('click', function () {
+    $('#offcanvas').toggleClass('open');
   })
+
+  // 開啟 Mobile-Schedule
+  $('[data-toggle="mobileSchedule"]').on('click', function () {
+    $('#mobileSchedule').toggleClass('open');
+    console.log('有喔');
+  })
+
+  // 關閉 Mobile-Schedule
+  $('.icon-close-schedule').on('click', function () {
+    $('#mobileSchedule').toggleClass('open');
+  })
+
 })
