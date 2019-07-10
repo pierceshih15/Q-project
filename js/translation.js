45b4f308ff18e0d77514152975f0cc5e2816e688
pierce <pierceshih@gmail.com>
2019-07-10 14:20:36 +0800

    Updated TextTranslation.

---
+++ js/translation.js
@@ -0,0 +1,165 @@
+// 中英文切換
+let arrLang = {
+  'en': {
+    'home': 'Home',
+    'q-name': 'Quincy Davis',
+    'about-dae': 'About DAE',
+    'gallery': 'Gallery',
+    'contact': 'Contact',
+    // 第一層
+    "opp-header": 'The opportunity for Taiwanese youth',
+    "opp-content": 'Trust in me to lead our Taiwanese youth on an inspirational journey abroad using sports and education!',
+    // 第二層
+    "qd-header": 'Quincy Davis III',
+    "qd-content": 'Travel to the United States of America with their leader Quincy Davis, the National Taiwanese basketball star',
+    // 第三層
+    "dae-content": 'Travel to the United States of America with their leader Quincy Davis, the National Taiwanese basketball star',
+    // 第四層
+    "more": 'More',
+    // 第五層
+    "schedule": 'Schedule',
+    "schedule-time": '25 Aug, 2019 - 17 Stp, 2019 15 days',
+    "schedule-location": 'California, San Diego',
+    "download": 'Download',
+    // 第六層
+    "reference-content-1": '“ Learn so much in this trip! I love you Quincy Davis! ”',
+    "reference-person-1": 'Name, Age School',
+    "reference-content-2": '“ Learn so much in this trip! I love you Quincy Davis! ”',
+    "reference-person-2": 'Name, Age School',
+    "reference-content-3": '“ Learn so much in this trip! I love you Quincy Davis! ”',
+    "reference-person-3": 'Name, Age School',
+    // 第七層
+    "form-header": 'You don’t want to miss it!',
+    "form-apply": 'Apply Format',
+    "form-consult": 'Personal Consultation',
+    "form-application": 'Filling Application',
+    "form-ready": 'Ready to go',
+    "form-name": 'Name',
+    "form-gender": 'Gender',
+    "form-email": 'Email',
+    "form-phone": 'Phone',
+    "form-number": '# of kids',
+    "form-age": 'Age',
+    "form-send": 'Send',
+    // 第八層
+    "sponsors": 'Sponsors',
+    "sponsors-content": 'Join sponsorship to help our youth.',
+
+    // Galley Page
+    "photos": 'Photos',
+    "videos": 'Videos',
+    "sports": 'Sports',
+    "science": 'Science',
+    "tour": 'Tour',
+
+    // Speakers Page
+    "intro-header": 'Title',
+    "intro-content": 'Travel to the United States of America with their leader QuincyDavis, the National Taiwanese basketball star.',
+
+    // 第三層
+    "speech-header": 'Speech',
+    "speech-content": 'Book Davis for your next high school, elementary, college speech.',
+    "location-1": 'PCSH',
+    "location-2": 'PCSH',
+    "location-3": 'PCSH',
+    "location-4": 'PCSH',
+    "location-5": 'PCSH',
+    "location-6": 'PCSH',
+
+    // 第四層
+    "team-intro": 'Speakers, coaches and trainers',
+    "speaker-name-1": 'Marcus Kinzer',
+    "speaker-intro-1": 'As a senior: Led his team in assists (119, 4.57 per game, 4th in C-USA), free throw percentage (.845, 4th in C-USA) and steals (37). Also ranked eighth in C-USA in assist-to-turnover ratio (1.72).',
+    "speaker-name-2": 'Don Singleterry',
+    "speaker-intro-2": 'Swish aspires to create a community of partners who work together with us to teach, inspire and improve the lives of children in Taiwan in sports and in the classroom. We are working with schools, businesses, and people of like mind & purpose to promote this balance of school and play to the participants of our programs.',
+    "speaker-name-3": 'Dr. Tanis King Starck',
+    "speaker-intro-3-1": 'Assistant Dean of Special Projects, SDSU College of Education Director, Cultural Proficiency Institute for Educators.',
+    "speaker-intro-3-2": 'As a noted Psychologist and Educational Administrator, and currently the Assistant Dean for Special Projects, College of Education at San Diego State University, Dr. Tanis King Starck has over 20 years of proven educational and administrative leadership in secondary and postsecondary education.',
+  },
+  'zh': {
+    'home': '首頁',
+    'q-name': '戴維斯',
+    'about-dae': '關於探索學院',
+    'gallery': '精彩留念',
+    'contact': '立即申請',
+    // 第一層
+    "opp-header": '台灣青少年的嶄新機會',
+    "opp-content": '相信我能帶領著孩子們，透過運動和教育，體驗更多精彩的海外旅程！',
+    // 第二層
+    "qd-header": '戴維斯 III',
+    "qd-content": '戴維斯，美裔台灣人，為籃球運動員。生於美國加州洛杉磯，後於2013年歸化中華民國國籍並放棄美國國籍，成為第一位透過歸化程序而加入中華台北代表隊選手的運動員。',
+    // 第三層
+    "dae-content": '戴維斯，美裔台灣人，為籃球運動員。生於美國加州洛杉磯，後於2013年歸化中華民國國籍並放棄美國國籍，成為第一位透過歸化程序而加入中華台北代表隊選手的運動員。',
+    // 第四層
+    "more": '更多',
+    // 第五層
+    "schedule": '行程表',
+    "schedule-time": '2019年8月25日到9月7日，共15天。',
+    "schedule-location": '加州聖地牙哥 ',
+    "download": '下載',
+    // 第六層
+    "reference-content-1": '“ 在這趟旅程學習到了好多經歷，真的很喜歡戴維斯！”',
+    "reference-person-1": '名字, 15歲 光復國中',
+    "reference-content-2": '“ 在這趟旅程學習到了好多經歷，真的很喜歡戴維斯！”',
+    "reference-person-2": '名字, 15歲 光復國中',
+    "reference-content-3": '“ 在這趟旅程學習到了好多經歷，真的很喜歡戴維斯！”',
+    "reference-person-3": '名字, 15歲 光復國中',
+    // 第七層
+    "form-header": '你絕對不會想讓孩子錯過的旅程！',
+    "form-apply": '線上申請',
+    "form-consult": '專人諮詢',
+    "form-application": '表格填寫',
+    "form-ready": '準備出發',
+    "form-name": '姓名',
+    "form-gender": '性別',
+    "form-email": '電子郵件',
+    "form-phone": '手機號碼',
+    "form-number": '申請孩童數',
+    "form-age": '年齡',
+    "form-send": '送出',
+    // 第八層
+    "sponsors": '贊助商',
+    "sponsors-content": '加入我們，成為孩子們的助力',
+
+    // Galley Page
+    "photos": '照片',
+    "videos": '影片',
+    "sports": '運動',
+    "science": '科學',
+    "tour": '旅遊',
+
+    // Speakers Page
+    "intro-header": '介紹',
+    "intro-content": '戴維斯，美裔台灣人，為籃球運動員。生於美國加州洛杉磯，後於2013年歸化中華民國國籍並放棄美國國籍，成為第一位透過歸化程序而加入中華台北代表隊選手的運動員。',
+
+    // 第三層
+    "speech-header": '校園演講',
+    "speech-content": '大專院校、國高中、小學，邀請戴維斯前往你的學校演講。',
+    "location-1": '板橋高中',
+    "location-2": '板橋高中',
+    "location-3": '板橋高中',
+    "location-4": '板橋高中',
+    "location-5": '板橋高中',
+    "location-6": '板橋高中',
+    "team-intro": '講者、教練及訓練員',
+
+    // 第四層
+    "team-intro": 'Speakers, coaches and trainers',
+    "speaker-name-1": 'Marcus Kinzer',
+    "speaker-intro-1": '作為一名大四生： 帶領他的球隊助攻（ 119 場比賽， 每場比賽4 .57 次， C - USA排名第4）， 罰球命中率（ .845， C - USA排名第4） 和搶斷（ 37 次）。 在輔助轉換率（ 1.72） 中， C - USA也排名第八。',
+    "speaker-name-2": 'Don Singleterry',
+    "speaker-intro-2": '我們希望建立一個合作夥伴社區，與我們一起工作，教育，激勵和改善台灣兒童在體育和課堂上的生活。我們正在與學校，企業以及有同樣想法和目的的人們合作，以促進學校的平衡，並為我們課程的參與者提供遊戲。',
+    "speaker-name-3": 'Dr. Tanis King Starck',
+    "speaker-intro-3-1": '聖地亞哥州立大學教育學院特殊項目助理院長，教育者文化能力研究所所長。',
+    "speaker-intro-3-2": '作為著名的心理學家和教育管理員，現任聖地亞哥州立大學教育學院特殊項目助理院長，Tanis King Starck博士在中學和高等教育方面擁有20多年的教育和行政領導經驗。',
+  },
+}
+
+$(function () {
+  $('.translation').click(function () {
+    var lang = $(this).attr('id');
+    $('.lang').each(function (index, element) {
+      $(this).text(arrLang[lang][$(this).attr('key')])
+    })
+  })
+})
