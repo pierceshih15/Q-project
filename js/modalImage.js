5860760946ed8ecdc7124d62fc41666c6c157759
pierce <pierceshih@gmail.com>
2019-07-30 13:30:06 +0800

    Add modal img feature for gallery page.

---
+++ js/modalImage.js
@@ -0,0 +1,22 @@
+const img = document.getElementsByClassName('myImg')
+const close = document.getElementsByClassName('close')
+
+// 顯示圖片
+for (let i = 0; i < img.length; i++) {
+  img[i].addEventListener('click', function (e) {
+    // Get the modal
+    let targetImg = e.target.nextElementSibling;
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    targetImg.style.display = "block";
+    targetImg.children[1].src = e.target.src;
+  })
+}
+
+// 關閉圖片
+for (let i = 0; i < close.length; i++) {
+  close[i].addEventListener('click', function (e) {
+    // 指定的 Modal
+    let el = e.target.parentElement;
+    el.style.display = "none";
+  })
+}
