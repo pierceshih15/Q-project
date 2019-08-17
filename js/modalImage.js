f730b45e505f0317eed546c82cfe2a65cdd2361b
pierce <pierceshih@gmail.com>
2019-08-17 15:41:22 +0800

    Modify Gallery page for image display

--- js/modalImage.js
+++ js/modalImage.js
@@ -5,7 +5,7 @@ const close = document.getElementsByClassName('close')
 for (let i = 0; i < img.length; i++) {
   img[i].addEventListener('click', function (e) {
     // Get the modal
-    let targetImg = e.target.nextElementSibling;
+    let targetImg = e.target.parentElement.nextElementSibling;
     // Get the image and insert it inside the modal - use its "alt" text as a caption
     targetImg.style.display = "block";
     targetImg.children[1].src = e.target.src;
@@ -16,7 +16,7 @@ for (let i = 0; i < img.length; i++) {
 for (let i = 0; i < close.length; i++) {
   close[i].addEventListener('click', function (e) {
     // 指定的 Modal
-    let el = e.target.parentElement;
+    let el = e.target.parentElement.parentElement;
     el.style.display = "none";
   })
 }
